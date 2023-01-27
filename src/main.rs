36f006e698edde900a550c72e1986d4934b943a0
Facundo Ernesto Ortega <ortegafacundo955@gmail.com>
2023-01-27 19:28:31 -0300

    Primer commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use chrono::Duration;
+
+fn main() {
+    let _time = chrono::Local::now().format("%H%M%S").to_string();
+    let treinta_minutos_mas = chrono::Local::now()
+        .checked_add_signed(Duration::minutes(30))
+        .expect("No se pudo calcular el dia")
+        .format("%H%M%S")
+        .to_string();
+    loop {
+        let time = chrono::Local::now().format("%H%M%S").to_string();
+        if &treinta_minutos_mas.get(..2).unwrap().parse::<isize>().unwrap() > &time.get(..2).unwrap().parse::<isize>().unwrap() {
+            let restante = (&treinta_minutos_mas.parse::<isize>().unwrap() - &time.parse::<isize>().unwrap()) / 100 - 40;
+            if restante == 0 {
+                println!("Se termino el tiempo");
+                break;
+            } else {
+                println!("Minutos restantes: {}", restante);
+            }
+        } else {
+            let restante = (&treinta_minutos_mas.parse::<isize>().unwrap() - &time.parse::<isize>().unwrap()) / 100;
+            if restante == 0 {
+                println!("Se termino el tiempo");
+                break;
+            } else {
+                println!("Minutos restantes: {}", restante);
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_secs(60));
+    }
+}
