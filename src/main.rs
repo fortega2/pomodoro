ecb012026f209697952a8910f37ffff538fa683a
Facundo Ernesto Ortega <ortegafacundo955@gmail.com>
2023-01-28 13:04:23 -0300

    Add argument for time calculation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use chrono::Duration;
 
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    let tiempo = args.get(1).expect("No se pudo calcular el tiempo").parse::<i64>().expect("No se puede tranformar una letra a número");
+    
     let treinta_minutos_mas = chrono::Local::now()
-        .checked_add_signed(Duration::minutes(30))
+        .checked_add_signed(Duration::minutes(tiempo))
         .expect("No se pudo calcular el dia")
         .format("%H%M%S")
         .to_string();
+
     loop {
         let time = chrono::Local::now().format("%H%M%S").to_string();
-        if &treinta_minutos_mas.get(..2).unwrap().parse::<isize>().unwrap() > &time.get(..2).unwrap().parse::<isize>().unwrap() {
-            let restante = (&treinta_minutos_mas.parse::<isize>().unwrap() - &time.parse::<isize>().unwrap()) / 100 - 40;
+
+        if treinta_minutos_mas.get(..2).unwrap().parse::<isize>().unwrap() > time.get(..2).unwrap().parse::<isize>().unwrap() {
+            let restante = (treinta_minutos_mas.parse::<isize>().unwrap() - time.parse::<isize>().unwrap()) / 100 - 40;
             if restante == 0 {
                 println!("Se termino el tiempo");
                 break;
@@ -17,7 +22,7 @@ fn main() {
                 println!("Minutos restantes: {}", restante);
             }
         } else {
-            let restante = (&treinta_minutos_mas.parse::<isize>().unwrap() - &time.parse::<isize>().unwrap()) / 100;
+            let restante = (treinta_minutos_mas.parse::<isize>().unwrap() - time.parse::<isize>().unwrap()) / 100;
             if restante == 0 {
                 println!("Se termino el tiempo");
                 break;
@@ -25,6 +30,7 @@ fn main() {
                 println!("Minutos restantes: {}", restante);
             }
         }
+
         std::thread::sleep(std::time::Duration::from_secs(60));
     }
 }
