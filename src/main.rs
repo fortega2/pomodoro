8668cad7519ad15d94e6ef3ce73eba045082d77a
Facundo Ernesto Ortega <ortegafacundo955@gmail.com>
2023-01-28 22:11:47 -0300

    Realease program

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,85 @@
-use chrono::Duration;
+use std::process::Command;
+use clap::{Parser};
+
+/// Programa que utiliza la gestión de tiempo llamada "tecnica pomodoro"
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Pomodoro {
+    /// Horas opcionales en numeros
+    #[arg(short = 'o', long)]
+    horas: Option<isize>,
+    /// Minutos en numeros
+    #[arg(short, long)]
+    minutos: isize,
+    /// Segundos en numeros
+    #[arg(short, long)]
+    segundos: isize,
+}
+
+fn cls() {
+    if cfg!(target_os = "windows") {
+        let _ = Command::new("cmd").args(["/C", "cls"]).status();
+    }
+}
 
 fn main() {
-    let args = std::env::args().collect::<Vec<String>>();
-    let tiempo = args.get(1).expect("No se pudo calcular el tiempo").parse::<i64>().expect("No se puede tranformar una letra a número");
+    let pomodoro = Pomodoro::parse();
     
-    let treinta_minutos_mas = chrono::Local::now()
-        .checked_add_signed(Duration::minutes(tiempo))
-        .expect("No se pudo calcular el dia")
-        .format("%H%M%S")
-        .to_string();
+    let mut _horas = 0;
+    let mut minutos = 0;
+    let mut segundos = 0;
+    
+    if let Some(horas) = pomodoro.horas {
+        _horas = horas;
+    }
+
+    if pomodoro.minutos > 59 {
+        panic!("Los minutos no pueden ser mayores a 59");
+    }
+
+    if pomodoro.segundos > 60 {
+        panic!("Los segundos no pueden ser mayores a 60");
+    }
+
+    minutos = pomodoro.minutos;
+    segundos = pomodoro.segundos;
 
     loop {
-        let time = chrono::Local::now().format("%H%M%S").to_string();
 
-        if treinta_minutos_mas.get(..2).unwrap().parse::<isize>().unwrap() > time.get(..2).unwrap().parse::<isize>().unwrap() {
-            let restante = (treinta_minutos_mas.parse::<isize>().unwrap() - time.parse::<isize>().unwrap()) / 100 - 40;
-            if restante == 0 {
-                println!("Se termino el tiempo");
-                break;
-            } else {
-                println!("Minutos restantes: {}", restante);
-            }
+        if _horas != 0 {
+            cls();
+            println!("Tiempo restante: Horas: {} Minutos: {} Segundos: {}", _horas, minutos, segundos);
         } else {
-            let restante = (treinta_minutos_mas.parse::<isize>().unwrap() - time.parse::<isize>().unwrap()) / 100;
-            if restante == 0 {
-                println!("Se termino el tiempo");
-                break;
+            if _horas == 0 && minutos != 0 {
+                cls();
+                println!("Tiempo restante: Minutos: {} Segundos: {}", minutos, segundos);
             } else {
-                println!("Minutos restantes: {}", restante);
-            }
+                cls();
+                println!("Tiempo restante: Segundos: {}", segundos);
+            }         
         }
 
-        std::thread::sleep(std::time::Duration::from_secs(60));
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        
+        segundos -= 0;
+
+        if _horas <= 0 && minutos <= 0 && segundos == 1 {
+            cls();
+            println!("Se terminó el tiempo");
+            break;
+        } else {
+            if segundos == 1 {
+                minutos -= 1;
+                segundos = 60;
+            } else {
+               segundos -= 1; 
+            }
+            
+            if minutos == 0 && _horas != 0 {
+                _horas -= 1;
+                minutos = 60;
+                segundos = 60;
+            }
+        }
     }
 }
